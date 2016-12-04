389c8f4166d52e406c99f8b30a089d13cc9f4824
Aljosha Novakovic <ollynov@gmail.com>
2016-12-04 15:49:53 -0800

    Successfully appending divs to clock with jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,51 @@ $(function() {
 
   var now = moment().format("hhmmssdA");
 
-  console.log('digit holder is: ', digitHolder);
+  $.each(positions, function() {
+
+    if (this === ':') {
+      digitHolder.append('<div class="dots">');
+    } else {
+
+      var pos = $('<div>');
+
+      for (var i = 1; i < 8; i++) {
+        pos.append('<span class="d' + i + '">');
+        console.log(pos);
+      }
+
+      // Set the digits as key:value pairs in the digits object
+      digits[this] = pos;
+
+      // Add the digit elements to the page
+      digitHolder.append(pos);
+    }
+  });
+
+  (function updateTime() {
+
+    // Use moment.js to output the current time as a string
+    // hh is for the hours in 12-hour format,
+    // mm - minutes, ss-seconds (all with leading zeroes),
+    // d is for day of week and A is for AM/PM
+
+    var now = moment().format("hhmmssdA");
+
+    digits.h1.attr('class', digitsSpelledOut[now[0]]);
+    digits.h2.attr('class', digitsSpelledOut[now[1]]);
+    digits.m1.attr('class', digitsSpelledOut[now[2]]);
+    digits.m2.attr('class', digitsSpelledOut[now[3]]);
+    digits.s1.attr('class', digitsSpelledOut[now[4]]);
+    digits.s2.attr('class', digitsSpelledOut[now[5]]);
+
+    console.log('here is now ', now);
+    // // Set the am/pm text:
+    // ampm.text(now[7]+now[8]);
+
+    // Schedule this function to be run again in 1 sec
+    setTimeout(updateTime, 1000);
+
+  })();
 
 });
 
